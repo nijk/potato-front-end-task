3afcf9f5899c30f62a4f555a4e6580854a5cc4f2
nijk <nick@codenamenick.co.uk>
2016-03-09 00:44:58 +0000

    Remove unnecessary imports from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,8 @@ import { Component, ViewEncapsulation } from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES } from 'angular2/router';
 
 import { FeedComponent } from './feed/feed.component';
-import { FeedListComponent } from './feed/feed-list.component';
-import { FeedDetailComponent } from './feed/feed-detail.component';
 
-//import { FlickrPublicFeedService } from './flickr/flickr-public-feed.service.ts';
+import { FlickrPublicFeedService } from './flickr/flickr-public-feed.service.ts';
 
 @RouteConfig([
     {
@@ -25,8 +23,8 @@ import { FeedDetailComponent } from './feed/feed-detail.component';
     template: '<router-outlet></router-outlet>',
     styles: [ require('./styles/skeleton/skeleton.scss') ],
     encapsulation: ViewEncapsulation.None,
-    directives: [ ROUTER_DIRECTIVES ]/*,
-    providers: [ FlickrPublicFeedService ]*/
+    directives: [ ROUTER_DIRECTIVES ],
+    providers: [ FlickrPublicFeedService ]
 })
 
 export class AppComponent {
